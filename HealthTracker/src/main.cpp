4db61db914045224ff1b2b8990e64518621fdb90
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-05-12 16:31:58 -0600

    first little program on my Microcontroller

--- HealthTracker/src/main.cpp
+++ HealthTracker/src/main.cpp
@@ -1,18 +1,48 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// // put function declarations here:
+// int myFunction(int, int);
+
+// void setup() {
+//   // put your setup code here, to run once:
+//   int result = myFunction(2, 3);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
+
+
+int count = 0;
+bool running = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    Serial.println("Send START to begin, STOP to pause");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (Serial.available()) {
+        String command = Serial.readStringUntil('\n');
+        command.trim();
+        if (command == "START") {
+            running = true;
+            Serial.println("\nStarted\n");
+        }
+        else if (command == "STOP") {
+            running = false;
+            Serial.println("\nStopped\n");
+            count = 0;
+        }
+    }
+    if (running) {
+        Serial.println(count);
+        count++;
+        delay(1000);
+    }
 }
